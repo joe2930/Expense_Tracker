b05619a92ca9064819dfee37d560988c2d8cd98a
joe2930 <madhurajyothivk.21cse@kongu.edu>
2024-02-14 15:02:06 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Expense Tracker/",
+  base:"/Expense_Tracker/",
   plugins: [react()],
 })
